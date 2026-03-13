af974d9d5e769b0283ff45dda2e0d4a15624820e
anelkica <anelkobarucija@gmail.com>
2026-03-13 13:33:19 +0100

    Add custom titlebar, Add Outfit TTF font

--- Tenner.Avalonia/Views/MainWindow.axaml.cs
+++ Tenner.Avalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,13 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Chrome;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Metadata;
+using Avalonia.Platform;
+using Lucide.Avalonia;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Tenner.Avalonia.Views
 {
@@ -7,6 +16,25 @@ namespace Tenner.Avalonia.Views
         public MainWindow()
         {
             InitializeComponent();
+            SetupTitlebar();
+        }
+
+        private void SetupTitlebar()
+        {
+            void UpdateMaximizeIcon()
+            {
+                MaximizeButton.Content = WindowState == WindowState.Maximized 
+                    ? new LucideIcon { Kind = LucideIconKind.SquaresUnite, Size = 16 } 
+                    : new LucideIcon { Kind = LucideIconKind.Square, Size = 16 };
+            }
+
+            Titlebar.PointerPressed += (_, e) => BeginMoveDrag(e);
+
+            MinimizeButton.Click += (_, _) => WindowState = WindowState.Minimized;
+            MaximizeButton.Click += (_, _) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal  : WindowState.Maximized;
+
+            PropertyChanged += (_, e) => { if (e.Property == WindowStateProperty) UpdateMaximizeIcon(); };
+            CloseButton.Click += (_, _) => Close();
         }
     }
 }
